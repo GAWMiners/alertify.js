5cde06295b1e60a8dced410d069217852fe6c7ea
Evan Lucas <evanlucas@me.com>
2014-06-15 22:41:58 -0500

    Remove use of debug

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var alertify = exports
-//  , debug = require('//debug')('alertify')
 
 var template = '<article class="alertify-log{{class}}">{{message}}</article>'
 
@@ -10,20 +9,17 @@ alertify.container = undefined
 alertify.extend = function(type) {
   if ('string' !== typeof type)
     throw new Error('type is required and must be a string')
-  //debug('extend:%s', type)
   return function(message, wait) {
     alertify.log(message, type, wait)
   }
 }
 
 function init() {
-  //debug('init')
   document.createElement('nav')
   document.createElement('article')
   document.createElement('section')
 
   if ($('alertify-logs') == null) {
-    //debug('creating container')
     alertify.container = document.createElement('section')
     alertify.container.setAttribute('id', 'alertify-logs')
     alertify.container.className = 'alertify-logs alertify-logs-hidden'
@@ -32,7 +28,6 @@ function init() {
 
   document.body.setAttribute('tabindex', '0')
   // transition
-  ////debug('getting transition')
   alertify.transition = getTransitionEvent()
 }
 
@@ -45,7 +40,6 @@ alertify.success = function(message, wait) {
 }
 
 alertify.log = function(message, type, wait) {
-  //debug('log - message %s type %s wait %s', message, type, wait)
   var check = function() {
     if (alertify.container && alertify.container.scrollTop !== null)
       return
@@ -60,7 +54,6 @@ alertify.log = function(message, type, wait) {
 }
 
 function notify(message, type, wait) {
-  //debug('notify - message %s type %s wait %s', message, type, wait)
   var log = document.createElement('article')
   log.className = 'alertify-log' + ((type && 'string' === typeof type)
     ? ' alertify-log-'+type
@@ -69,7 +62,6 @@ function notify(message, type, wait) {
   log.innerHTML = message
   alertify.container.appendChild(log)
   setTimeout(function() {
-    //debug('showing notification')
     log.className = log.className + ' alertify-log-show'
   }, 50)
 
@@ -81,32 +73,24 @@ function close(ele, wait) {
     ? +wait
     : 5000
 
-  //debug('setup close %s', wait)
   bind(ele, 'click', function() {
-    //debug('bound click')
     hide(ele)
   })
 
   function transitionDone(event) {
-    //debug('begin transitionDone')
     event.stopPropagation()
-    //debug('unbinding event')
     unbind(this, alertify.transition.type, transitionDone)
     alertify.container.removeChild(this)
     if (!alertify.container.hasChildNodes())
       alertify.container.className += ' alertify-logs-hidden'
-    //debug('end transitionDone')
   }
 
   function hide() {
-    //debug('hide')
     if ('undefined' !== typeof ele && ele.parentNode === alertify.container) {
       if (alertify.transition.supported) {
-        //debug('transition supported, binding to done event')
         bind(ele, alertify.transition.type, transitionDone)
         ele.className += ' alertify-log-hide'
       } else {
-        //debug('transition not supported, removing log')
         alertify.container.removeChild(ele)
         if (!alertify.container.hasChildNodes())
           alertify.container.className += ' alertify-logs-hidden'
@@ -117,7 +101,6 @@ function close(ele, wait) {
   if (wait === 0) return
 
   setTimeout(function() {
-    //debug('timer done')
     hide()
   }, timer)
 }
@@ -125,7 +108,6 @@ function close(ele, wait) {
 // Helpers
 
 function bind(ele, event, fn) {
-  //debug('bind %s', event)
   if ('function' === typeof ele.addEventListener) {
     ele.addEventListener(event, fn, false)
   } else if (ele.attachEvent) {
@@ -134,7 +116,6 @@ function bind(ele, event, fn) {
 }
 
 function unbind(ele, event, fn) {
-  //debug('unbind %s', event)
   if ('function' === typeof ele.removeEventListener) {
     ele.removeEventListener(event, fn, false)
   } else if (ele.detachEvent) {
